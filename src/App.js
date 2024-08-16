afce129a3ce6b5bbabb1ec60cf96fd3a0c3bd7a7
Vineel Arepalli <2200039088@kluniversity.in>
2024-08-16 12:33:38 +0530

    Aug 16

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Student from './Student';
 
 const App = () => {
-  useEffect(() => {
-    // Function to handle context menu event (right-click)
+ /*   useEffect(() => {
+    Function to handle context menu event (right-click)
     const handleContextMenu = (event) => {
       event.preventDefault(); // Prevent the default right-click behavior
     };
@@ -20,7 +20,7 @@ const App = () => {
     return () => {
       document.removeEventListener('contextmenu', handleContextMenu);
     };
-  }, []);
+  }, []); */
 
   return (
     <Student />
